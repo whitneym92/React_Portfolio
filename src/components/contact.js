4a94b0332b1eb302189f44c63ef2f14efdef4f87
Whitney Monk <whitneym92@gmail.com>
2020-10-01 17:18:32 -0500

    updated styles

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,6 +4,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInbox } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
+const styles = {
+    layout: {
+        position: "relative",
+        background: "linear-gradient(to right, #355c7d, #6c5b7b, #c06c84)",
+        zIndex: 0,
+    },
+    heading: {
+        position: "relative",
+        top: -300,
+        textAlign: "center",
+        color: "black",
+        fontFamily: "Raleway sans-serif",
+    },
+    desc: {
+        position: "relative",
+        top: -350,
+        textAlign: "center",
+        background: "#d9d9ed",
+        boxShadow: "10px 5px #d46fbb",
+        border: "0 1px solid #d46fbb",
+        marginLeft: "10em",
+        marginRight: "10em",
+        padding: 20,
+        zIndex: -1,
+        fontFamily: "Montserrat sans-serif",
+    },
+    icons: {
+        position: "relative",
+        top: -250,
+        left: 300,
+        display: "inline",
+        marginLeft: "1em",
+        marginRight: "1em",
+        justifyContent: "center",
+        color: "#ffffff",
+    }
+}
 class Contact extends Component {
     render() {
         return(
