707a59907a0a839b3bde0d1619cc51ac25f655c2
Whitney Monk <whitneym92@gmail.com>
2020-10-01 17:34:16 -0500

    added styles to home

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,22 @@ import { Grid, Cell } from 'react-mdl';
 import avatar from '../assets/avatAR.jpg';
 import { Spring } from 'react-spring/renderprops';
 
+const styles={
+    layout: {
+        top:0
+    },
+    design: {
+        background: "linear-gradient(to right, #355c7d, #6c5b7b, #c06c84)",
+        height: "500px",
+        textAlign: "center",
+    },
+    avatar: {
+        height: 250,
+        position: "relative",
+        paddingTop: "5em",
+        borderRadius: 200,
+    },
+}
 
 class Home extends Component {
     render() {
