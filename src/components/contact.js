5ed554cc63e4d0f9b2afcae3d953681c4fbccc95
Whitney Monk <whitneym92@gmail.com>
2020-10-01 17:09:09 -0500

    added contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { Spring } from 'react-spring/renderprops';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInbox } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 class Contact extends Component {
     render() {
         return(
-            <div><h1>Contact</h1></div>
+            <Spring
+            from={{ opacity:0 }}
+            to={{ opacity:1 }}
+            config={{ delay: 3000, duration: 2000, }}
+            >
+                {props => (
+                    <div style={props}>
+                        <div style={styles.layout}>
+                            <h1 style={styles.heading}>Let's Talk</h1>
+                            <h4>style={styles.desc}>I'm always looking to chat, collaborate, network and just talk code</h4>
+
+                        </div>
+                        <div>
+                            <a href="mailto:whitneym92@gmail.com?" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInbox} size="5x" style={styles.icons} /></a>
+                            <a href="https://github.com/whitneym92" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} size="5x" style={styles.icons} /></a>
+                            <a href="https://www.linkedin.com/in/whitney-monk/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedinIn} size="5x" style={styles.icons} /></a>
+
+                        </div>
+                    </div>
+                )}
+            </Spring>
+
         )
     }
 }
